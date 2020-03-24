6f6be79856986f9ffa80f5be2f3d2140b2a92c06
Flo Orford <floorford@flos-mbp.mynet>
2020-03-24 12:05:02 +0000

    installed bycrypt for nodejs and created a prehook save for the user model which encrypts the password before saving to db

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,32 @@
+const User = require("../models/user");
+
 exports.signup = function(req, res, next) {
-  res.send({ success: true });
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: "You must provide an email and password" });
+  }
+
+  // mongoose function
+  User.findOne({ email: email }, function(err, existingUser) {
+    // i.e. cannot connect to DB
+    if (err) return next(err);
+
+    if (existingUser) {
+      // unprocessable entity
+      return res.status(422).send({ error: "Email is in use" });
+    }
+
+    const user = new User({ email: email, password: password });
+
+    user.save(function(err) {
+      // i.e. cannot connect to DB
+      if (err) return next(err);
+
+      res.json({ success: true });
+    });
+  });
 };
--- models/user.js
+++ models/user.js
@@ -6,14 +6,42 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 // how we tell mongoose what the fields our model has
 
+const bcrypt = require("bcrypt-nodejs");
+
 // Define model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
 });
 
+// On Save Hook, encrypt password
+// runs before saving model
+userSchema.pre("save", function(next) {
+  // get access to user model
+  // (user = instance of user model)
+  const user = this;
+
+  // using bcrypt library genSalt function
+  // passing in callback to ensure synchronisity
+  bcrypt.genSalt(10, function(err, salt) {
+    // error handling
+    if (err) return next(err);
+
+    // hashes password using salt
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+
+      // overwrite plain text with encrypted version
+      user.password = hash;
+
+      // go ahead save model, prehook finished
+      next();
+    });
+  });
+});
+
 // Create model class
-const ModelClass = mongoose.Model("user", userSchema);
+const ModelClass = mongoose.model("user", userSchema);
 
 // export model
-module.exports(ModelClass);
+module.exports = ModelClass;
